ac01a6f1a46f5b2a61cc4a06ae62262fa9bc0a41
awfeequdng <291458254@qq.com>
2020-12-17 23:18:25 +0800

    1、实现wrap和unwrap函数，并且unwrap函数测试通过； 2、在unwrap函数的实现中，需要特别注意不要让最后计算出来的unwrap值小于0，因为absolute seq是不会回绕的；因此在计算得到checkpoint附近的一个值小于0时，直接丢弃该值； 3、(checkpoint%(1ul << 32))这个值需要和step（seqno步进的序列数）以及step + (1ul << 32)进行比较，离哪个近就向哪边移动checkpoint的值。

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,5 +1,5 @@
 #include "wrapping_integers.hh"
-
+#include <iostream>
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -14,8 +14,8 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint32_t seqno = (n + isn.raw_value())% UINT32_MAX;
+    return WrappingInt32{seqno};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +29,30 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    int32_t diff = n - isn;
+    uint64_t step{};
+    if (diff < 0) {
+        step = diff + (1ul << 32);
+    } else {
+        step = diff;
+    }
+    uint64_t remain = checkpoint % (1ul << 32);
+    uint64_t left_dis;
+    uint64_t right_dis;
+    if (remain >= step) {
+        left_dis = (remain - step);
+        right_dis = (1ul << 32) - left_dis;
+    } else {
+        right_dis = (step - remain);
+        left_dis = (1ul << 32) - right_dis;
+    }
+    // 保证checkpoint必须大于left_dis
+    if (checkpoint < left_dis) {
+        checkpoint += right_dis;
+    } else if (right_dis <= left_dis) {
+        checkpoint += right_dis;
+    } else {
+        checkpoint -= left_dis;
+    }
+    return checkpoint;
 }
