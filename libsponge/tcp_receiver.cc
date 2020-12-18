90327212426378fe5f08776da9d5dad74e9baa40
awfeequdng <291458254@qq.com>
2020-12-18 13:55:04 +0800

    lab2: 1、测试通过recv_window 2、在设置_next_ackno时，我们需要考虑乱序的情况，不能直接将当前数据包的seqno + length_in_sequence_space作为_next_ackno; 3、根据first_unassembled反推出_next_ackno，    first_unassembled_index就是下一个希望接收的index，根据_syn_fin_cnt转换成next_ackno    这样反推可能存在一个问题：    当发送数据包的顺序如下：syn -> data1 -> data2 -> fin，当这个data1和data2数据包都丢失后，    发送数据包的顺序可能就是: syn -> data1 -> data2 -> fin -> data1 -> data2,    接收端看到的数据包可能就是: syn -> fin -> data1 -> data2，    那么此时从接收端看来，data1回应ack时_syn_fin_cnt等于2了(本应该等于1，因为在正常的有序的顺序来看data1前面只有syn数据包)，    但此时由于数据包乱序后，data1比fin晚到接收段，导致data1接收到时，syn 和fin都接收到了,从而_syn_fin_cnt等于2。    然后通过first_unassembled_index以及_syn_fin_cnt反推出_next_ackno时，_next_ackno就比实际的值要大1。    因此这段代码不能使用(虽然能通过recv_window的测试用例，但对乱序的考虑不足，还存在bug) 4、当接收到的segment不是syn以及之前也没有接收到syn时，将这个segment直接丢弃。

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -29,6 +29,9 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
     if (seg.header().fin) {
         eof = true;
     }
+    // 如果不是syn数据包，或者在这之前没有接收到过syn数据包，当前的这个数据包就是无效的，直接丢弃
+    if (!_isn_flag)
+        return;
 
     // 计算出absolute seqno
     uint64_t abs_seqno = unwrap(seg.header().seqno, WrappingInt32(_isn), _reassembler.first_unassembled_index());
@@ -39,8 +42,6 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
     std::string push_string(std::string(seg.payload().str()));
     _reassembler.push_substring(push_string, index, eof);
     
-    _next_ackno = seg.header().seqno.raw_value() + seg.length_in_sequence_space();
-
     // 从absolute seqno中减去syn和fin的个数，就是bytestream中index的值;
     // 如果当前这个数据包中也包含syn和fin，则当前index不能减去这个数据包对syn和fin的计数，
     // 当前的syn和fin只会影响下一个数据包的index，不会影响当前数据包的index。
@@ -48,6 +49,22 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
         _syn_fin_cnt++;
     if (seg.header().fin)
         _syn_fin_cnt++;
+
+    // 根据first_unassembled反推处_next_ackno
+    // first_unassembled_index就是下一个希望接收的index，根据_syn_fin_cnt转换成next_ackno
+    // 这样反推可能存在一个问题：
+    // 当发送数据包的顺序如下：syn -> data1 -> data2 -> fin，当这个data1和data2数据包都丢失后，
+    // 发送数据包的顺序可能就是: syn -> data1 -> data2 -> fin -> data1 -> data2,
+    // 接收端看到的数据包可能就是: syn -> fin -> data1 -> data2，
+    // 那么此时从接收端看来，data1回应ack时_syn_fin_cnt等于2了(本应该等于1，因为在正常的有序的顺序来看data1前面只有syn数据包)，
+    // 但此时由于数据包乱序后，data1比fin晚到接收段，导致data1接收到时，syn 和fin都接收到了,从而_syn_fin_cnt等于2。
+    // 然后通过first_unassembled_index以及_syn_fin_cnt反推出_next_ackno时，_next_ackno就比实际的值要大1。
+    // 因此这段代码不能使用(虽然能通过recv_window的测试用例，但对乱序的考虑不足，还存在bug)
+    index = _reassembler.first_unassembled_index();
+    abs_seqno = index + _syn_fin_cnt;
+    _next_ackno = wrap(abs_seqno, WrappingInt32(_isn)).raw_value();
+    // 这段代码默认认为数据包是顺序到达的，这有一定问题
+    // _next_ackno = seg.header().seqno.raw_value() + seg.length_in_sequence_space();
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const { 
